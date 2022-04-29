c53258c0d777f36a77915af268ffdfb1569d3915
trubiso <trubiso@users.noreply.github.com>
2022-04-30 01:48:14 +0200

    h

--- crear_c_ping.py
+++ crear_c_ping.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 from time import sleep
 from typing import Any, Optional
 from discord import *
@@ -7,6 +8,7 @@ import random
 import os
 import time
 import asyncio
+import string
 
 intents = discord.Intents.default()
 intents.members = True
@@ -21,16 +23,6 @@ zgdps = 950574511550504962 # esto es noches locas; zgdps = 729104446948376707
 
 texto = "En 1725 se ordenó diácono y tres años después pasó a formar parte del clero de la Iglesia de Inglaterra. En 1729 se trasladó a Oxford como miembro de la junta directora del Lincoln College, donde fundó junto a su hermano Charles el Holy Club, en el que ingresó también George Whitefield, futuro fundador del metodismo calvinista.\n\nEn 1735 se traslada a Estados Unidos como misionero anglicano y en el viaje conoce a unos alemanes de Moravia cuya sencilla devoción evangélica le impresionó. Durante su estancia en Georgia siguió tratándolos y tradujo algunos de sus himnos al inglés. Regresó a su país en 1738 y el 24 de mayo, mientras esperaba un encuentro con los moravos en la calle Aldersgate, en Londres, experimentó un despertar religioso que le convenció de que cualquier persona podía alcanzar la salvación sólo con tener fe en Jesucristo.\n\nEn marzo de 1739, George Whitefield, entonces famoso predicador en Bristol, lo llamó para que unieran sus esfuerzos. En un principio se negó a predicar fuera de las iglesias, pero la entusiasta reacción de la audiencia tras el sermón que pronunció el 2 de abril al aire libre lo convenció de que era la forma más efectiva de llegar a las masas."
 
-async def limitar_pings():
-    # pings 1 minuto, 4 minutos descanso, todo para evitar el timeout de discord
-    global pingueando
-
-    pingueando = True
-    await asyncio.sleep(60)
-
-    pingueando = False
-    await asyncio.sleep(60 * 4)
-
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="la Preqle"))
@@ -41,57 +33,15 @@ async def on_ready():
 
     print('Bot is ready')
 
-@bot.event
-async def on_message(ctx: Optional[commands.Context] = None):
-    global pingueando
-    global everyoneCount
-    global messagesToDelete
-    global zgdps
-
-    if not pingueando: return
-    # sleep(everyoneCount / 10000)
-    for m in messagesToDelete:
-        try:
-            messagesToDelete.remove(m)
-            await m.delete()
-        except BaseException:
-            continue
-
     zgdps_server = bot.get_guild(zgdps)
-    if not zgdps_server: return
-    zgdps_channels = zgdps_server.channels
-    text_channels = [c for c in zgdps_channels if isinstance(c, TextChannel)]
+    ch = bot.get_channel(969746552333680641)
+    if not ch: return
+    cat = ch.category
+    for channel in cat.channels:
+      n = f"{''.join(random.choice(string.ascii_letters) for i in range(32)).replace(string.whitespace, '')[:16]}-{channel.name[:3]}"
+      print(f"Creando canal #{n}")
+      await cat.create_text_channel(name=n)
 
-    channel = random.choice(text_channels)
-    messagesToDelete.append(await channel.send(f"@everyone <@&950893604820353065> {texto} (NOCHE LOCA UWUWUWUWU)"))
-    everyoneCount += 1
-    print(f"{everyoneCount} everyones")
-
-    #await bot.get_channel(878434221004316732).send("@everyone")
-    #if not ctx.author.bot:
-        #await ctx.reply("@everyone holiwis 😊😊😊")
-    # if '68' in ctx.content:
-    #     h = bot.get_emoji(787817120515620884)
-    #     try:
-    #         await ctx.add_reaction(h)
-    #     except BaseException:
-    #         pass
-    # if '69' in ctx.content:
-    #     h = bot.get_emoji(810291162974912522)
-    #     try:
-    #         await ctx.add_reaction(h)
-    #     except BaseException:
-    #         pass
-
-    # todo este codigo ha sido quitado para poder facilitar pings
+    exit()
 
 bot.run(os.environ['BOT_TOKEN'])
-
-while True:
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(limitar_pings())
-    loop.close()
-
-    loop2 = asyncio.get_event_loop()
-    loop2.run_until_complete(on_message())
-    loop2.close()
